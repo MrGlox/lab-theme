5fe7aba43d0110b6c3391b6c79d416702cf09e30
MrGlox <contact@morgan-leroux.com>
2019-04-09 23:51:22 +0200

    clean repository and init main scss

--- resources/assets/scripts/main.js
+++ resources/assets/scripts/main.js
@@ -1,14 +1,11 @@
 // import external dependencies
-import 'jquery';
-
-// Import everything from autoload
-import './autoload/**/*'
+import 'jquery'
 
 // import local dependencies
-import Router from './util/Router';
-import common from './routes/common';
-import home from './routes/home';
-import aboutUs from './routes/about';
+import Router from './util/Router'
+import common from './routes/common'
+import home from './routes/home'
+import aboutUs from './routes/about'
 
 /** Populate Router instance with DOM routes */
 const routes = new Router({
@@ -18,7 +15,7 @@ const routes = new Router({
   home,
   // About Us page, note the change from about-us to aboutUs.
   aboutUs,
-});
+})
 
 // Load Events
-jQuery(document).ready(() => routes.loadEvents());
+jQuery(document).ready(() => routes.loadEvents())
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,9 @@
     <div class="wrap container" role="document">
       <div class="content">
         <main class="main">
-          @yield('content')
+          <div class="container">
+            @yield('content')
+          </div>
         </main>
         @if (App\display_sidebar())
           <aside class="sidebar">
