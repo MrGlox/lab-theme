a77029be98478de5abb7cb3b0e4cc791ed292b6e
MrGlox <contact@morgan-leroux.com>
2019-04-10 21:56:46 +0200

    stabilize all stack

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,18 +5,18 @@
     @php do_action('get_header') @endphp
     @include('partials.header')
     <div class="wrap container" role="document">
-      <div class="content">
-        <main class="main">
-          <div class="container">
-            @yield('content')
-          </div>
-        </main>
-        @if (App\display_sidebar())
-          <aside class="sidebar">
-            @include('partials.sidebar')
-          </aside>
-        @endif
-      </div>
+      <main class="main">
+        <div class="content">
+            <div class="container">
+              @yield('content')
+            </div>
+          <!-- @if (App\display_sidebar())
+            <aside class="sidebar">
+              @include('partials.sidebar')
+            </aside>
+          @endif -->
+        </div>
+      </main>
     </div>
     @php do_action('get_footer') @endphp
     @include('partials.footer')
